77ac9a420c56090c9dd0adf259f620ee75f3a85e
axiomaswn <yomaswn@gmail.com>
2017-06-19 14:56:45 +0700

    inherit evenemmit

--- index.js
+++ index.js
@@ -4,9 +4,29 @@ var events = require('events');
 // Create an eventEmitter object
 var eventEmitter = new events.EventEmitter();
 
+var util = require('util');
 
-eventEmitter.on('clicked', (buttonName) => {
+var Students = function(name){
+ this.name = name;
+}
+
+eventEmitter.on('clicked', () => {
+  console.log('something is clicked');
+})
+eventEmitter.emit('clicked');
+
+eventEmitter.on('clicked2', (buttonName) => {
   console.log(`${buttonName} is clicked`);
 })
+eventEmitter.emit('clicked2','button1');
 
-eventEmitter.emit('clicked','button1');
+//inherit eventEmitter pada Students. Students bisa menggunakan eventEmitter juga
+util.inherits(Students, events.EventEmitter);
+
+var max = new Students('max');
+
+max.on('marks', function(nilai){
+  console.log(`${this.name} nilainya adalah ${nilai}`);
+})
+
+max.emit('marks', 90);
